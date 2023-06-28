4c97ff78ee3c8ac976386b456af31791e31fbfe0
Jufri Ali <faqat0_0@hotmail.com>
2023-06-28 22:29:56 +0800

    created appcontent component

---
+++ src/components/AppContent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function AppContent() {
+  return <div>This is coming from the App Content!</div>;
+}
+
+export default AppContent;
--- src/components/Button.js
+++ src/components/Button.js
@@ -7,7 +7,7 @@ const buttonTypes = {
   secondary: 'secondary',
 };
 
-function Button({ children, type, variant }) {
+function Button({ children, type, variant, ...rest }) {
   return (
     <button
       className={getClasses([
@@ -15,10 +15,23 @@ function Button({ children, type, variant }) {
         styles[`button--${buttonTypes[variant]}`],
       ])}
       type={type === 'submit' ? 'submit' : 'button'}
+      {...rest}
     >
       {children}
     </button>
   );
 }
 
+function SelectButton({ children, ...rest }) {
+  return (
+    <select
+      className={getClasses([styles.button, styles.button__select])}
+      {...rest}
+    >
+      {children}
+    </select>
+  );
+}
+
+export { SelectButton };
 export default Button;
