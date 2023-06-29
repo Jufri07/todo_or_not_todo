faee97612cf509542527d4ffe020a7b322c4683f
Jufri Ali <faqat0_0@hotmail.com>
2023-06-29 10:05:56 +0800

    form status label

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import Button, { SelectButton } from './Button';
 import styles from '../styles/modules/app.module.scss';
+import TodoModal from './TodoModal';
 
 function AppHeader() {
   return (
     <div className={styles.appHeader}>
-      <Button variant="primary">Click Me</Button>
+      <Button variant="primary">Add Task</Button>
       <SelectButton id="status">
         <option value="all">All</option>
         <option value="incomplete">Incomplete</option>
         <opton value="complete">Complete</opton>
       </SelectButton>
+      <TodoModal />
     </div>
   );
 }
