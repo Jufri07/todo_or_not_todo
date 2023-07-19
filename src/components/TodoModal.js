755294af9991c6fda48b457bcc5522f95ad0ad7c
Jufri Ali <faqat0_0@hotmail.com>
2023-07-19 22:41:38 +0800

    local storage

--- src/components/TodoModal.js
+++ src/components/TodoModal.js
@@ -1,50 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MdOutlineClose } from 'react-icons/md';
 import styles from '../styles/modules/modal.module.scss';
 import Button from './Button';
 
 function TodoModal({ modalOpen, setModalOpen }) {
+  const [title, setTitle] = useState('');
+  const [status, setStatus] = useState('incomplete');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({ title, status });
+  };
   return (
-    <div>
-      {modalOpen && (
-        <div className={styles.wrapper}>
-          <div className={styles.container}>
-            <div
-              className={styles.closeButton}
-              onClick={() => setModalOpen(false)}
-              onKeyDown={() => setModalOpen(false)}
-              tabIndex={0}
-              role="button"
-            >
-              <MdOutlineClose />
-            </div>
-            <form className={styles.form}>
-              <h1 className={styles.formTitle}>Add Task</h1>
-              <label htmlFor="title">
-                Task Title
-                <input type="text" id="title" />
-              </label>
-              <label htmlFor="status">
-                Task Status
-                <select name="status" id="status">
-                  <option value="incomplete">Incomplete</option>
-                  <option value="complete">Complete</option>
-                  <option value="kiv">KIV</option>
-                </select>
-              </label>
-              <div className={styles.buttonContainer}>
-                <Button type="submit" variant="primary">
-                  Add Your Task
-                </Button>
-                <Button type="button" variant="primary">
-                  Cancel Task!
-                </Button>
-              </div>
-            </form>
+    modalOpen && (
+      <div className={styles.wrapper}>
+        <div className={styles.container}>
+          <div
+            className={styles.closeButton}
+            onClick={() => setModalOpen(false)}
+            onKeyDown={() => setModalOpen(false)}
+            tabIndex={0}
+            role="button"
+          >
+            <MdOutlineClose />
           </div>
+          <form className={styles.form} onSubmit={(e) => handleSubmit(e)}>
+            <h1 className={styles.formTitle}>Add Task</h1>
+            <label htmlFor="title">
+              Task Title
+              <input
+                type="text"
+                id="title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
+            </label>
+            <label htmlFor="status">
+              Task Status
+              <select
+                name="status"
+                id="status"
+                value={status}
+                onChange={(e) => setStatus(e.target.value)}
+              >
+                <option value="incomplete">Incomplete</option>
+                <option value="complete">Complete</option>
+                <option value="kiv">KIV</option>
+              </select>
+            </label>
+            <div className={styles.buttonContainer}>
+              <Button type="submit" variant="primary">
+                Add Your Task
+              </Button>
+              <Button
+                type="button"
+                variant="secondary"
+                onClick={() => setModalOpen(false)}
+                onKeyDown={() => setModalOpen(false)}
+              >
+                Cancel Task!
+              </Button>
+            </div>
+          </form>
         </div>
-      )}
-    </div>
+      </div>
+    )
   );
 }
 
