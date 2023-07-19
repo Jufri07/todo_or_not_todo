ab8f653fa2947d8159c7d69884f69e8a36aec192
Jufri Ali <faqat0_0@hotmail.com>
2023-07-19 21:49:06 +0800

    useState added

--- src/components/TodoModal.js
+++ src/components/TodoModal.js
@@ -1,30 +1,49 @@
 import React from 'react';
 import { MdOutlineClose } from 'react-icons/md';
 import styles from '../styles/modules/modal.module.scss';
+import Button from './Button';
 
-function TodoModal() {
+function TodoModal({ modalOpen, setModalOpen }) {
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.container}>
-        <div className={styles.closeButton}>
-          <MdOutlineClose />
+    <div>
+      {modalOpen && (
+        <div className={styles.wrapper}>
+          <div className={styles.container}>
+            <div
+              className={styles.closeButton}
+              onClick={() => setModalOpen(false)}
+              onKeyDown={() => setModalOpen(false)}
+              tabIndex={0}
+              role="button"
+            >
+              <MdOutlineClose />
+            </div>
+            <form className={styles.form}>
+              <h1 className={styles.formTitle}>Add Task</h1>
+              <label htmlFor="title">
+                Task Title
+                <input type="text" id="title" />
+              </label>
+              <label htmlFor="status">
+                Task Status
+                <select name="status" id="status">
+                  <option value="incomplete">Incomplete</option>
+                  <option value="complete">Complete</option>
+                  <option value="kiv">KIV</option>
+                </select>
+              </label>
+              <div className={styles.buttonContainer}>
+                <Button type="submit" variant="primary">
+                  Add Your Task
+                </Button>
+                <Button type="button" variant="primary">
+                  Cancel Task!
+                </Button>
+              </div>
+            </form>
+          </div>
         </div>
-        <form className={styles.form}>
-          <h1 className={styles.formTitle}>Add Task</h1>
-          <label htmlFor="title">
-            Title HOi HOi
-            <input type="text" id="title" />
-          </label>
-          <label htmlFor="status">
-            Status Hoi Hoi
-            <select name="status" id="status">
-              <option value="incomplete">Incomplete</option>
-              <option value="complete">Complete</option>
-              <option value="kiv">KIV</option>
-            </select>
-          </label>
-        </form>
-      </div>
+      )}
     </div>
   );
 }
